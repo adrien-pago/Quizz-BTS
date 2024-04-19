e177b52b5b1b3e4aac599a3a36d3dbd01eeaba9d
DevTest <115026138+adrien-pago@users.noreply.github.com>
2024-04-19 10:48:46 +0200

    Update (Adrien)

--- Code Source modif Projet/Quizz/frmQuizz.Designer.cs
+++ Code Source modif Projet/Quizz/frmQuizz.Designer.cs
@@ -31,19 +31,19 @@ namespace Quizz
             this.Classement = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtPseudo
             // 
-            this.txtPseudo.Location = new System.Drawing.Point(378, 26);
+            this.txtPseudo.Location = new System.Drawing.Point(523, 46);
             this.txtPseudo.Name = "txtPseudo";
             this.txtPseudo.Size = new System.Drawing.Size(100, 22);
             this.txtPseudo.TabIndex = 0;
-            this.txtPseudo.TextChanged += new System.EventHandler(this.txtPseudo_TextChanged);
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(378, 68);
+            this.txtPassword.Location = new System.Drawing.Point(523, 89);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.Size = new System.Drawing.Size(100, 22);
             this.txtPassword.TabIndex = 1;
@@ -55,7 +55,7 @@ namespace Quizz
             this.cmdAjouterLePseudo.Cursor = System.Windows.Forms.Cursors.Hand;
             this.cmdAjouterLePseudo.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.cmdAjouterLePseudo.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.cmdAjouterLePseudo.Location = new System.Drawing.Point(270, 152);
+            this.cmdAjouterLePseudo.Location = new System.Drawing.Point(415, 176);
             this.cmdAjouterLePseudo.Name = "cmdAjouterLePseudo";
             this.cmdAjouterLePseudo.Size = new System.Drawing.Size(208, 102);
             this.cmdAjouterLePseudo.TabIndex = 2;
@@ -65,7 +65,7 @@ namespace Quizz
             // 
             // cmdDebut
             // 
-            this.cmdDebut.Location = new System.Drawing.Point(293, 163);
+            this.cmdDebut.Location = new System.Drawing.Point(437, 187);
             this.cmdDebut.Name = "cmdDebut";
             this.cmdDebut.Size = new System.Drawing.Size(162, 81);
             this.cmdDebut.TabIndex = 4;
@@ -77,7 +77,7 @@ namespace Quizz
             // 
             this.cmdSignUp.BackColor = System.Drawing.Color.Lime;
             this.cmdSignUp.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.cmdSignUp.Location = new System.Drawing.Point(270, 111);
+            this.cmdSignUp.Location = new System.Drawing.Point(415, 135);
             this.cmdSignUp.Name = "cmdSignUp";
             this.cmdSignUp.Size = new System.Drawing.Size(208, 35);
             this.cmdSignUp.TabIndex = 3;
@@ -89,18 +89,18 @@ namespace Quizz
             // 
             this.Classement.BackColor = System.Drawing.Color.Gainsboro;
             this.Classement.ItemHeight = 16;
-            this.Classement.Location = new System.Drawing.Point(37, 26);
+            this.Classement.Location = new System.Drawing.Point(38, 52);
             this.Classement.Name = "Classement";
-            this.Classement.Size = new System.Drawing.Size(177, 228);
+            this.Classement.Size = new System.Drawing.Size(297, 228);
             this.Classement.TabIndex = 4;
-            this.Classement.SelectedIndexChanged += new System.EventHandler(this.lstClassement_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(267, 32);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(412, 52);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 16);
+            this.label1.Size = new System.Drawing.Size(70, 16);
             this.label1.TabIndex = 5;
             this.label1.Text = "PSEUDO";
             this.label1.Click += new System.EventHandler(this.cmdAjouterLePseudo_Click);
@@ -108,17 +108,31 @@ namespace Quizz
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(267, 74);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(412, 92);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(86, 16);
+            this.label2.Size = new System.Drawing.Size(94, 16);
             this.label2.TabIndex = 6;
             this.label2.Text = "PASSWORD";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Firebrick;
+            this.label3.Location = new System.Drawing.Point(34, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(301, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Classement des meilleures parties";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // frmQuizz
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(523, 279);
+            this.ClientSize = new System.Drawing.Size(650, 329);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtPseudo);
@@ -137,6 +151,7 @@ namespace Quizz
         private Label label1;
         private Label label2;
         private System.Windows.Forms.Button cmdDebut;
+        private Label label3;
     }
 }
 
